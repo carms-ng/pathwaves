76e10ab2e816cd0b684c709310ea3c7a0f5dea38
Carms Ng <contact@carms-ng.com>
2021-09-29 16:05:25 -0400

    [ADD] Calendar and Resource Placeholder Component

---
+++ src/components/Calendar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Calendar() {
+  return(
+    <h1
+      style={{minHeight: `90vh`}}
+    >Calendar Secret Content</h1>
+  )
+}
---
+++ src/components/Resource.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Resource() {
+  return(
+    <h1
+      style={{minHeight: `90vh`}}
+    >Secret Resource Content</h1>
+  )
+}
