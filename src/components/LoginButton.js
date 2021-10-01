3684639095a150834cad54acd07f5ef5cfd26642
Carms Ng <contact@carms-ng.com>
2021-10-01 15:00:37 -0400

    [ADD] login logout buttons

---
+++ src/components/LoginButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LoginButton = ({ className }) => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button
+      className={className}
+      type="button"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
+};
+
+export default LoginButton;
