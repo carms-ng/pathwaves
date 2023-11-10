f1b12ca8809ff78c25730da192b8cfd678291876
Carms Ng <carmsng@Carmss-MBP.lan>
2023-11-10 12:58:41 -0500

    chore: eslint add rule for quote & autoformat

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,25 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ["react"],
   rules: {
-    'react/jsx-filename-extension': [
+    "react/jsx-filename-extension": [
       1,
       {
-        extensions: [
-          '.js',
-          '.jsx',
-        ],
+        extensions: [".js", ".jsx"],
       },
     ],
-    'no-param-reassign': 0,
-    'react/prop-types': 0,
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    "no-param-reassign": 0,
+    "react/prop-types": 0,
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    quotes: [2, "double", { avoidEscape: true }],
   },
 };
