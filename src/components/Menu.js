9d984f24dff956b1718b058a36462734ad9126a5
Carms Ng <carmsng@Carmss-MBP.lan>
2023-11-10 17:54:55 -0500

    feat: show contact btn, hide auth menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,19 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React from 'react';
-import styled from 'styled-components';
-import LanguageSwitcher from './LanguageSwitcher';
-import LoginButton from './LoginButton';
-import MenuAuth from './MenuAuth';
-import NavItem from './NavItem';
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import styled from "styled-components";
+import LanguageSwitcher from "./LanguageSwitcher";
+import LoginButton from "./LoginButton";
+import MenuAuth from "./MenuAuth";
+import NavItem from "./NavItem";
 
 export default function Menu({
-  isMenuOpen, items, lang, slug, menuAuth,
+  isMenuOpen,
+  items,
+  lang,
+  slug,
+  showAuthMenu,
+  menuAuth,
+  btnContact,
 }) {
   const { isAuthenticated } = useAuth0();
   return (
@@ -23,9 +29,30 @@ export default function Menu({
           slug={slug}
         />
       ))}
-      {isAuthenticated
-        ? <MenuAuth lang={lang} slug={slug} menuAuth={menuAuth} btnClassName="btn btn-main" />
-        : <LoginButton lang={lang} label={menuAuth.labelLogin} className="btn btn-main" />}
+
+      <a
+        type="button"
+        href={`mailto:${btnContact.mailTo}`}
+        className="btn btn-main"
+      >
+        {btnContact.label}
+      </a>
+
+      {showAuthMenu &&
+        (isAuthenticated ? (
+          <MenuAuth
+            lang={lang}
+            slug={slug}
+            menuAuth={menuAuth}
+            ClassName="btn btn-main"
+          />
+        ) : (
+          <LoginButton
+            lang={lang}
+            label={menuAuth.labelLogin}
+            className="btn btn-main"
+          />
+        ))}
 
       <LanguageSwitcher lang={lang} slug={slug} />
     </MenuStyles>
@@ -35,7 +62,7 @@ export default function Menu({
 const MenuStyles = styled.div`
   z-index: 11;
   background: var(--white);
-  box-shadow: 0px 0px 20px rgba(0,0,0,0.2);
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
   position: fixed;
   top: 0;
   left: 0;
@@ -46,7 +73,7 @@ const MenuStyles = styled.div`
   display: grid;
   transition: var(--slow);
   justify-items: flex-start;
-  transform: ${(props) => (props.isMenuOpen ? '' : 'translate(0, -100%)')};
+  transform: ${(props) => (props.isMenuOpen ? "" : "translate(0, -100%)")};
   transition: var(--trans);
   width: 100%;
   grid-gap: 1rem;
